12980a09089384452936ed903fc89eb518545313
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2015-10-04 20:59:10 +0200

    Basic is_cell_alive implementation.
    Using HashSet to store the info about alive cells, and only alive cells.

--- rust/game_of_life/src/main.rs
+++ rust/game_of_life/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+
+fn is_cell_alive(world: HashSet<(i8,i8)>, cell_coordinates: (i8,i8)) -> bool {
+    return world.contains(&cell_coordinates);
+}
+
+#[test]
+fn is_cell_alive_should_return_true_for_alive_cell(){
+    let coords = (0,0);
+    let mut world: HashSet<(i8,i8)> = HashSet::new();
+    world.insert(coords);
+    let is_alive = is_cell_alive(world,coords);
+    assert_eq!(true, is_alive);
+}
+
+#[test]
+fn is_cell_alive_should_return_false_for_dead_cell(){
+    let coords = (0,0);
+    let world: HashSet<(i8,i8)> = HashSet::new();
+    let is_alive = is_cell_alive(world,coords);
+    assert_eq!(false, is_alive);
 }
