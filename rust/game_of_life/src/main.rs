7f833643d2122cd8cd2a55fc8d342e8a7e6ee18c
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2015-10-07 19:28:35 +0200

    Refactored to make WorldMap immutable.

--- rust/game_of_life/src/main.rs
+++ rust/game_of_life/src/main.rs
@@ -6,13 +6,19 @@ struct WorldMap{
 
 impl WorldMap{
     
-    fn new() -> WorldMap {
-        WorldMap { map: HashSet::new() }
+    fn empty() -> WorldMap {
+        return WorldMap::new( HashSet::new() );
     }
-   
-    // TODO: make self immutable here, return a new copy of World
-    fn mark_cell_as_alive(& mut self, cell_coordinates: (i8, i8)) {
-        self.map.insert(cell_coordinates);
+    
+    fn new(new_map: HashSet<(i8,i8)>) -> WorldMap {
+        return WorldMap { map: new_map };
+    }
+
+    fn mark_cell_as_alive(&self, cell_coordinates: (i8, i8)) -> WorldMap {
+        let mut new_map = self.map.clone();
+        new_map.insert(cell_coordinates);
+        let new_world = WorldMap::new(new_map); 
+        return new_world;
     }
 
     fn is_cell_alive(&self, cell_coordinates: (i8,i8)) -> bool {
@@ -24,16 +30,16 @@ impl WorldMap{
 #[test]
 fn is_cell_alive_should_return_true_for_alive_cell(){
     let coords = (0,0);
-    let mut world = WorldMap::new();
-    world.mark_cell_as_alive(coords);
-    let is_alive = world.is_cell_alive(coords);
+    let world = WorldMap::empty();
+    let new_world = world.mark_cell_as_alive(coords);
+    let is_alive = new_world.is_cell_alive(coords);
     assert_eq!(true, is_alive);
 }
 
 #[test]
 fn is_cell_alive_should_return_false_for_dead_cell(){
     let coords = (0,0);
-    let world = WorldMap::new();
+    let world = WorldMap::empty();
     let is_alive = world.is_cell_alive(coords);
     assert_eq!(false, is_alive);
 }
