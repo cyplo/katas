bb25609f8bb87aa46c1a13a3297b7afe64b6f3a5
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2015-10-07 19:04:01 +0200

    Extracted WorldMap type.
    In game of life, Rust implementation, extracted out a type for
    WorldMap, where previously only function were used. Some TODOs added.

--- rust/game_of_life/src/main.rs
+++ rust/game_of_life/src/main.rs
@@ -1,22 +1,39 @@
 use std::collections::HashSet;
 
-fn is_cell_alive(world: HashSet<(i8,i8)>, cell_coordinates: (i8,i8)) -> bool {
-    return world.contains(&cell_coordinates);
+struct WorldMap{
+    map: HashSet<(i8, i8)>
+}
+
+impl WorldMap{
+    
+    fn new() -> WorldMap {
+        WorldMap { map: HashSet::new() }
+    }
+   
+    // make self immutable here, return a new copy of World
+    fn mark_cell_as_alive(& mut self, cell_coordinates: (i8, i8)) {
+        self.map.insert(cell_coordinates);
+    }
+
+    fn is_cell_alive(&self, cell_coordinates: (i8,i8)) -> bool {
+        return self.map.contains(&cell_coordinates);
+    }
+
 }
 
 #[test]
 fn is_cell_alive_should_return_true_for_alive_cell(){
     let coords = (0,0);
-    let mut world: HashSet<(i8,i8)> = HashSet::new();
-    world.insert(coords);
-    let is_alive = is_cell_alive(world,coords);
+    let mut world = WorldMap::new();
+    world.mark_cell_as_alive(coords);
+    let is_alive = world.is_cell_alive(coords);
     assert_eq!(true, is_alive);
 }
 
 #[test]
 fn is_cell_alive_should_return_false_for_dead_cell(){
     let coords = (0,0);
-    let world: HashSet<(i8,i8)> = HashSet::new();
-    let is_alive = is_cell_alive(world,coords);
+    let world = WorldMap::new();
+    let is_alive = world.is_cell_alive(coords);
     assert_eq!(false, is_alive);
 }
