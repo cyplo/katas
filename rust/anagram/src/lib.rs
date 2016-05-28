e3f217e05a4ba3ea975208db60c1d78e13498212
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-05-28 22:23:05 +0200

    case-insensitivy working

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -12,12 +12,12 @@ pub fn anagrams_for<'a>(base_string: &str, candidates: &[&'a str]) -> Vec<&'a st
 }
 
 fn is_not_same_word(lhs: &str, rhs: &str) -> bool {
-    lhs != rhs
+    lhs.to_lowercase() != rhs.to_lowercase()
 }
 
 fn has_all_letters(base: &str, candidate: &str) -> bool {
-    let mut available_letters = Vec::from_iter(candidate.chars());
-    for letter in base.chars() {
+    let mut available_letters = Vec::from_iter(candidate.to_lowercase().chars());
+    for letter in base.to_lowercase().chars() {
         let position = available_letters.iter()
             .position(|&tested_letter| tested_letter == letter);
 
