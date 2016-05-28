fce45a620d29cadba72670514da2d8ebd53b2d20
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-05-28 22:24:04 +0200

    all tests passing for anagram

--- rust/anagram/tests/anagram.rs
+++ rust/anagram/tests/anagram.rs
@@ -84,7 +84,6 @@ fn test_does_not_detect_a_differently_cased_word_as_its_own_anagram() {
 }
 
 #[test]
-#[ignore]
 fn test_does_not_detect_a_differently_cased_unicode_word_as_its_own_anagram() {
     let inputs = ["ΑΒγ"];
     let outputs: Vec<&str> = vec![];
