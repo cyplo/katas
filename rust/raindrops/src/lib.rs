ee89b448fe29a56f147151f542120fa2a13b8841
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-23 23:40:56 +0200

    raindrops 1 and 3 working

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,4 +1,15 @@
+extern crate primal;
 
-pub fn raindrops(_: i32) -> String {
-    String::from("1")
+use primal::Sieve;
+
+pub fn raindrops(x: usize) -> String {
+    let sieve_up_to = 2 << 16;
+    let sieve = Sieve::new(sieve_up_to);
+    let factors_as_tuples = sieve.factor(x).unwrap();
+    let mut factors = factors_as_tuples.iter().map( |tuple| tuple.0 );
+    let has3 = factors.any(|factor| factor == 3);
+    if has3 {
+        return "Pling".to_string();
+    }
+    return x.to_string();
 }
