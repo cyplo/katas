c0145b3d06904d9c78198c9a2a4ee0e7301f57be
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-23 23:54:42 +0200

    raindrops working for 3 and 5

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -6,10 +6,15 @@ pub fn raindrops(x: usize) -> String {
     let sieve_up_to = 2 << 16;
     let sieve = Sieve::new(sieve_up_to);
     let factors_as_tuples = sieve.factor(x).unwrap();
-    let mut factors = factors_as_tuples.iter().map( |tuple| tuple.0 );
-    let has3 = factors.any(|factor| factor == 3);
-    if has3 {
+    let factors: Vec<usize> = factors_as_tuples.iter().map( |tuple| tuple.0 ).collect();
+   
+    if factors.iter().any(|&factor| factor == 3) {
         return "Pling".to_string();
     }
+
+    if factors.iter().any(|&factor| factor == 5) {
+        return "Plang".to_string();
+    }
+
     return x.to_string();
 }
